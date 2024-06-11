1bfbdb37cf2fa8cb0dc2c21e90efc017291c0654
hiber-neet <hoangtvse172883@fpt.edu.vn>
2024-06-11 10:01:38 +0700

    update main code to use recursion

--- src/main/java/com/dino/math/util/core/MathUtility.java
+++ src/main/java/com/dino/math/util/core/MathUtility.java
@@ -25,9 +25,15 @@ public class MathUtility {
         if(n == 0 || n == 1){
             return 1;
         }
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+//        for (int i = 1; i <= n; i++) {
+//            product *= i;
+//        }
+        return n * getFactorial(n - 1);
+        //recursion, gọi lại chính mình với 1 quy mô khác
     }
+    
+    // 4! = 4 * 3!
+    //3! = 3 * 2!
+    //2! = 2 * 1!
+    //n! = n * (n - 1)! công thức đệ quy
 }
